e6b6e996ca3aa11329aefc5085d411f7152777ad
Vladimir Satdarov <vladimir.satdarov@gmail.com>
2024-01-18 13:06:44 +0100

    adding if condition of the day picker

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,13 +1,15 @@
 package pages.components;
 
-import pages.RegistrationPage;
-
 import static com.codeborne.selenide.Selenide.$;
 
 public class CalendarComponent {
     public void setDate(String day, String month, String year) {
         $(".react-datepicker__month-select").selectOptionByValue(month);
         $(".react-datepicker__year-select").selectOptionByValue(year);
-        $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
+        if (1 <= Integer.valueOf(day) && Integer.valueOf(day) <= 9){
+            $(".react-datepicker__day--00" + day + ":not(.react-datepicker__day--outside-month)").click();
+        } else {
+            $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
+        }
     }
 }
